86641aabe8eb481b57226b6709ceb3ad30d863b1
Sebastian <139753003+Iakobson@users.noreply.github.com>
2024-02-16 10:27:00 +0200

    Update page.tsx

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,10 @@
 // @file: /app/dashboard/customers/page.tsx
-export default function Page() {
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default function CustomersPage() {
   return <p>Customers Page</p>;
 }
